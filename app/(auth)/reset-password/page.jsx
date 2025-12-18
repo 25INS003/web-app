6827a6939329b388fb8a19d72d576bf00ac44296
Aryan-81 <llaryanllrajll@gmail.com>
2025-12-18 19:39:28 +0530

    --

--- app/(auth)/reset-password/page.jsx
+++ app/(auth)/reset-password/page.jsx
@@ -1,6 +1,6 @@
 "use client";
 import { useState } from "react";
-import { useAuthStore } from "@/store/useAuthStore";
+import { useAuthStore } from "@/store/authStore";
 import { useRouter } from "next/navigation";
 
 export default function ResetPassword() {
